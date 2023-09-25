22ff19d961ca2e08eb0de33a0f94f8cb997a4dda
felix <l836828946@gmail.com>
2023-09-25 17:57:17 +0800

    4.2

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@
 #define numVAOs 1
 #include "Utils.h"
 
+
 using namespace std;
 
-
 #define numVAOs 1
-#define numVBOs 2
+#define numVBOs 1
 
 //Utils util = Utils();
 float cameraX, cameraY, cameraZ;
@@ -23,13 +23,15 @@ GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // variable allocation for display
-GLuint mvLoc, projLoc;
+GLuint vLoc, tfLoc, projLoc;
+//GLuint mvLoc, projLoc;
 int width, height, displayLoopi;
 float aspect;
-float tf;
+float timeFactor;
 glm::mat4 pMat, vMat, tMat, rMat, mMat, mvMat;
 
 void setupVertices(void) {
+    // 12 triangles * 3 vertices * 3 values (x, y, z)
     float vertexPositions[108] = {
             -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,
             1.0f, -1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
@@ -49,11 +51,17 @@ void setupVertices(void) {
     glGenBuffers(numVBOs, vbo);  // creates VBO and returns the integer ID
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+
+    // loads the cube vertices into the 0th VBO buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
 
+// once
 void init (GLFWwindow* window) {
-    renderingProgram = Utils::createShaderProgram("../vertShader.glsl","../fragShader.glsl");
+    renderingProgram = Utils::createShaderProgram(
+            "../vertShader.glsl",
+            "../fragShader.glsl");
+
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians == 60 degrees
@@ -71,36 +79,34 @@ void display(GLFWwindow* window, double currentTime) {
 
     glUseProgram(renderingProgram);
 
-    // get locations of uniforms in the shader program
-    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
-    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
-
     vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
+    vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+    glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-    // tf == "time factor"
-    for (displayLoopi = 0; displayLoopi<24; displayLoopi++) {
-        tf = currentTime + displayLoopi;
-        tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(.35f*tf)*8.0f, cos(.52f*tf)*8.0f, sin(.70f*tf)*8.0f));
-        rMat = glm::rotate(glm::mat4(1.0f), 1.75f*tf, glm::vec3(0.0f, 1.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f*tf, glm::vec3(1.0f, 0.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f*tf, glm::vec3(0.0f, 0.0f, 1.0f));
-        mMat = tMat * rMat;
-        mvMat = vMat * mMat;
 
-        glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    timeFactor = ((float)currentTime);
+    tfLoc = glGetUniformLocation(renderingProgram, "tf");
+    glUniform1f(tfLoc, (float)timeFactor);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
-        glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(0);
 
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
 
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 240);
 }
+
+//void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
+//    aspect = (float)newWidth / (float)newHeight;
+//    glViewport(0, 0, newWidth, newHeight);
+//    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+//}
 
 int main(void) {
     if (!glfwInit()) {exit(EXIT_FAILURE);}
@@ -108,13 +114,16 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // I don't know what this does
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // and neither this
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program2", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter3 - exercise1", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) {exit(EXIT_FAILURE);}
     glfwSwapInterval(1);
 
+//    glfwSetWindowSizeCallback(window, window_size_callback);
+
     init(window);
 
+    // the rate at which display() is called is referred to as the frame rate
     while (!glfwWindowShouldClose(window)) {
         display(window, glfwGetTime());
         glfwSwapBuffers(window);
