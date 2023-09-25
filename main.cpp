0fb23498328deeb273a89328640d0df80e573c3e
felix <l836828946@gmail.com>
2023-09-25 16:50:54 +0800

    4.1

--- main.cpp
+++ main.cpp
@@ -9,44 +9,83 @@
 #define numVAOs 1
 #include "Utils.h"
 
-// GLuint : unsigned int
-GLuint renderingProgram;
-GLuint vao[numVAOs];
-// location of triangle on x axis
-float x = 0.0f;
-// offset for moving the triangle
-float inc = 0.01f;
-
 using namespace std;
 
 
+#define numVAOs 1
+#define numVBOs 2
+
+//Utils util = Utils();
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+// variable allocation for display
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
+
+void setupVertices(void) {
+    float vertexPositions[108] = {
+            -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f,  1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f,
+    };
+    glGenVertexArrays(1, vao);  // creates VAO and returns the integer ID
+    glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo);  // creates VBO and returns the integer ID
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
+
 void init (GLFWwindow* window) {
     renderingProgram = Utils::createShaderProgram("../vertShader.glsl","../fragShader.glsl");
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    setupVertices();
 }
 
 void display(GLFWwindow* window, double currentTime) {
-    // clear the background to black, each time
     glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(0.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
     glUseProgram(renderingProgram);
 
-    // move the triangle along x axis
-    x += inc;
-    // switch to moving the triangle to the left
-    if (x > 1.0f) inc = -0.01f;
-    // switch to moving the triangle to the right
-    if (x < -1.0f) inc = 0.01f;
+    // get locations of uniforms in the shader program
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-    // get ptr to "offset"
-    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
+    // send matrix data to the uniform variables
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians == 60 degrees
 
-    glProgramUniform1f(renderingProgram, offsetLoc, x);
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+    mvMat = vMat * mMat;
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);  // makes the 0th buffer "active"
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);  // associates 0th attribute with buffer
+    glEnableVertexAttribArray(0);  // enable the 0th vertex attribute
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
 int main(void) {
